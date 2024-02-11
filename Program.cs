50285068667f1f485c1a06ef8fcfdc8f1ca085f3
Nikita Malyshev Aorus <nikita110097@mail.ru>
2024-02-11 21:56:37 +0200

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text.Json;
+using System.Xml.Serialization;
+
+namespace ConverterFromJsonToXml
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var serialiser = new XmlSerializer(typeof(MyClass));
+
+            /*
+            MyClass s = new MyClass() { g = 5 };
+
+            
+            */
+
+            using (var streamJSON = File.OpenRead("serial.json"))
+            {
+
+                MyClass? newMyClass = JsonSerializer.Deserialize<MyClass>(streamJSON);
+
+                using (var stream = File.OpenWrite("serial1.xml"))
+                {
+                    serialiser.Serialize(stream, newMyClass);
+                }
+            }
+
+
+        }
+    }
+
+    public class MyClass
+    {
+        public string[] str = new string[] { "One", "Two", "Three" };
+        public int g = 1;
+
+        public IncludeMyClass include = new IncludeMyClass() { l = 50 };
+    }
+
+    public class IncludeMyClass
+    {
+        public long l = 15;
+    }
+
+}
